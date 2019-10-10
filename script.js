ed095a771f7d5d6e869b53ce5a2007e118a222fc
Marius Kuzminskas <33681369+MariusKuzminskas@users.noreply.github.com>
2019-10-10 23:05:25 +0300

    Bug fix, added unique Id and simpler checked state

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 var app = new Vue({
-	el: "#app",
-	data: {
-		todo: "add new todo",
-		todosArr: [],
-		classTest: true
-	},
-	methods: {
-		addToDoHandler: function(textInput) {
-			console.log(textInput);
-			this.todosArr.push({ text: textInput, checked: false });
-		},
-		selected: function(index) {
-			// itemState === false ? (itemState = false) : (itemState = true);
-			if (this.todosArr[index].checked == false) {
-				this.todosArr[index].checked = true;
-			} else {
-				this.todosArr[index].checked = false;
-			}
-			// console.log("item state after: ", this.todosArr[index].checked);
-		},
-		deleteMe: function(index) {
-			this.todosArr.splice(index, 1);
-		}
-	}
+  el: "#app",
+  data: {
+    todo: "",
+    todosArr: [],
+    mainIndex: 0
+  },
+  methods: {
+    addToDoHandler: function(textInput) {
+      console.log(textInput);
+      this.todosArr.push({
+        id: this.mainIndex,
+        text: textInput,
+        checked: false
+      });
+      this.mainIndex++;
+      this.todo = "";
+    },
+    deleteMe: function(index) {
+      this.todosArr.splice(index, 1);
+    }
+  }
 });
